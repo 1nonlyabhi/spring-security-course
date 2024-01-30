813c602a65351cee630fa57e7002b30ff3630076
1nonlyabhi <gabhishek0407@gmail.com>
2024-01-31 00:23:09 +0530

    postgres connection

---
+++ src/main/java/io/explorer/springsecuritycourse/user/User.java
@@ -0,0 +1,36 @@
+package io.explorer.springsecuritycourse.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetailsService {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
