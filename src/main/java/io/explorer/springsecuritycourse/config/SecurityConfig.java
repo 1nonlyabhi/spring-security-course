45a1ea70a33279432adae31f51b5006ae2a38e95
1nonlyabhi <gabhishek0407@gmail.com>
2024-01-30 23:04:47 +0530

    initalizing

---
+++ src/main/java/io/explorer/springsecuritycourse/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.explorer.springsecuritycourse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("foo")
+                .password("foo")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
