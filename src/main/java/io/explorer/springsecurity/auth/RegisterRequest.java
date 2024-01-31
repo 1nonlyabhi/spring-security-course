f3aedda2cec2094e102084804c8346c996f07bef
1nonlyabhi <gabhishek0407@gmail.com>
2024-01-31 23:40:41 +0530

    request and response entity added

---
+++ src/main/java/io/explorer/springsecurity/auth/RegisterRequest.java
@@ -0,0 +1,18 @@
+package io.explorer.springsecurity.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+}
